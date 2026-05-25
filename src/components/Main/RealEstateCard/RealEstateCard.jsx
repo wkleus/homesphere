08b105e1145cbfb7df06d8e8bf075f55645e7a29
WuKe <118019715+source-code-examples@users.noreply.github.com>
2026-05-25 12:23:35 +0200

    add home details with icons and tooltips

--- src/components/Main/RealEstateCard/RealEstateCard.jsx
+++ src/components/Main/RealEstateCard/RealEstateCard.jsx
@@ -3,12 +3,15 @@ import "./RealEstateCard.css";
 import RealEstatePhoto from "./RealEstatePhoto/RealEstatePhoto";
 import RealEstateCategory from "./RealEstatePhoto/RealEstateCategory/RealEstateCategory";
 import RealEstateStatus from "./RealEstatePhoto/RealEstateStatus/RealEstateStatus";
+import IconItem from "./RealEstatePhoto/IconItem/IconItem";
+import { Bed, Buildings, Lightning, Square } from "phosphor-react";
 
 const RealEstateCard = ({
   address,
   isAvailable,
   energyClass,
   rent,
+  buy,
   photo,
   rooms,
   squareMeters,
@@ -16,10 +19,28 @@ const RealEstateCard = ({
   yearBuilt,
 }) => {
   return (
-    <div className="realEstate-card" style={{ opacity: isAvailable ? 1 : 0.5 }}>
+    <div className="realEstate-card" style={{ opacity: isAvailable ? 1 : 0.6 }}>
       <RealEstatePhoto photo={photo}>
         <RealEstateCategory category={category} />
         {!isAvailable && <RealEstateStatus />}
+        <div className="home-details">
+          <IconItem Icon={Bed} description={rooms} tooltip="Number of rooms" />
+          <IconItem
+            Icon={Square}
+            description={`${squareMeters} m²`}
+            tooltip="Living area in square meters"
+          />
+          <IconItem
+            Icon={Lightning}
+            description={energyClass}
+            tooltip="Energy efficiency class"
+          />
+          <IconItem
+            Icon={Buildings}
+            description={yearBuilt}
+            tooltip="Year built"
+          />
+        </div>
       </RealEstatePhoto>
       <div>RealEstate Card Entries</div>
     </div>
--- src/components/Main/RealEstateCard/RealEstatePhoto/IconItem/IconItem.jsx
+++ src/components/Main/RealEstateCard/RealEstatePhoto/IconItem/IconItem.jsx
@@ -1,7 +1,11 @@
 import "./IconItem.css";
 
-const IconItem = () => {
-  return <span>IconItem</span>;
+const IconItem = ({ Icon, description, tooltip }) => {
+  return (
+    <span className="icon-item" title={tooltip}>
+      <Icon size={15} className="icon" /> {description}
+    </span>
+  );
 };
 
 export default IconItem;
