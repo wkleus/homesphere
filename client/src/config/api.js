1ac0bbdab7248fa05c4c1210c6a8c45031c6407b
WuKe <118019715+source-code-examples@users.noreply.github.com>
2026-06-03 02:31:40 +0200

    update API URL to correct Render deployment

--- client/src/config/api.js
+++ client/src/config/api.js
@@ -6,7 +6,7 @@
 // Determine the API base URL based on the environment.
 const API_BASE = import.meta.env.DEV
   ? "http://localhost:3000/api" // Local Development
-  : "https://homesphere-api.onrender.com/api"; // ← My Render URL
+  : "https://homesphere-kifc.onrender.com/api"; // ← My Render URL
 
 export const ENTRIES_URL = `${API_BASE}/entries`;
 export const ENTRY_URL = (id) => `${API_BASE}/entries/${id}`;
