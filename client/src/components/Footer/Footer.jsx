3e6ff468b5fb452cfa2161213867ba9a81a1c151
WuKe <118019715+source-code-examples@users.noreply.github.com>
2026-06-14 03:10:52 +0200

    add i18n to Footer and RealEstateStatus components

--- client/src/components/Footer/Footer.jsx
+++ client/src/components/Footer/Footer.jsx
@@ -1,13 +1,14 @@
 import { ClockAfternoon } from "phosphor-react";
 import "./Footer.css";
+import { useTranslation } from "react-i18next";
 
 const Footer = () => {
+  const { t } = useTranslation();
   const businessStart = 8;
   const businessEnd = 18;
   const curDay = new Date().getDay();
   const curTime = new Date().getHours();
 
-  // const storeIsOpen = true;
   const storeIsOpen =
     curDay > 0 &&
     curDay < 6 &&
@@ -18,10 +19,10 @@ const Footer = () => {
     <div>
       <div className="note">
         <ClockAfternoon className="clock-icon open" />
-        <span className="note open">Store is still open.</span>
+        <span className="note open">{t("footer.openNote")}</span>
       </div>
       <div>
-        Contact us at{" "}
+        {t("footer.contact")}{" "}
         <a className="email" href="mailto:example@example.com">
           example@example.com
         </a>
@@ -31,9 +32,9 @@ const Footer = () => {
     <div>
       <div className="note">
         <ClockAfternoon className="clock-icon closed" />
-        <span className="note closed">Store is closed.</span>
+        <span className="note closed">{t("footer.closedNote")}</span>
       </div>
-      <div>Business hours: 8am - 6pm, Mon - Fri</div>
+      <div>{t("footer.hours")}</div>
     </div>
   );
 
--- client/src/components/Main/RealEstateCard/RealEstatePhoto/RealEstateStatus/RealEstateStatus.jsx
+++ client/src/components/Main/RealEstateCard/RealEstatePhoto/RealEstateStatus/RealEstateStatus.jsx
@@ -1,9 +1,11 @@
 import "./RealEstateStatus.css";
+import { useTranslation } from "react-i18next";
 
 const RealEstateStatus = () => {
+  const { t } = useTranslation();
   return (
     <div>
-      <div className="status">Already taken</div>
+      <div className="status">{t("card.alreadyTaken")}</div>
     </div>
   );
 };
